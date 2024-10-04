705256176f70f421bedd746272e5b6ee643c80a8
Alden Cutler <aldencutler84@gmail.com>
2024-10-04 16:09:32 -0400

    add mongodb connection to server/main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,38 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import 'dotenv/config';
+import cookie from 'cookie-session';
+import { MongoClient, ObjectId, Collection } from 'mongodb';
 
 const app = express();
 
+// middleware
+app.use(express.static('src'))
+app.use(express.json())
+
+let users: Collection;
+async function run() {
+  const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
+  const client = new MongoClient(uri);
+
+  await client.connect();
+  users = client.db('AMAP').collection('users');
+}
+
+try {
+    run().catch(console.dir);
+    console.log("Connected to database");
+} catch {
+    console.log("Error connecting to database");
+}
+
+// ----------------- ROUTES -----------------
+// --- GET ---
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
+
+// --- POST ---
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
