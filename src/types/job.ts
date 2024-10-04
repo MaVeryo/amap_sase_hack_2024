51932051aebf7d448b59bfc40795a056e8a0478f
Alden Cutler <aldencutler84@gmail.com>
2024-10-04 19:24:56 -0400

    add user and job types

---
+++ src/types/job.ts
@@ -0,0 +1,12 @@
+export type Job = {
+    _id: string;
+    title: string;
+    company: string;
+    location: string;
+    salary: string;
+    description: string;
+    link: string;
+    created: string;
+    updated: string;
+    user: string;   // This is the user's _id
+}
