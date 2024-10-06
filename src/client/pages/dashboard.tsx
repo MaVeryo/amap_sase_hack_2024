f681c5d5ebe32fd1785f5c3f92db531cbcf7c206
pako490 <141786924+pako490@users.noreply.github.com>
2024-10-06 12:19:43 -0400

    added button to go to userProfile page

--- src/client/pages/dashboard.tsx
+++ src/client/pages/dashboard.tsx
@@ -26,6 +26,18 @@ async function getUserData() {
     }
 }
 
+async function toUserFunc(event: React.MouseEvent<HTMLButtonElement>, navigate: ReturnType<typeof useNavigate> ){
+    event.preventDefault();
+    const input = {
+        user: document.getElementById('username') as HTMLInputElement | null,
+        pass: document.getElementById('password') as HTMLInputElement | null
+    };
+
+    //response checker? 
+
+    navigate('/userprofile');
+}
+
 function Dashboard() {
     const navigate = useNavigate();
     const [ userData, setUserData ] = useState(null);
@@ -98,55 +110,17 @@ function Dashboard() {
             </>
         )}
         
-    </div>
+        </div>
+
+        <button 
+            id="toUser" 
+            type="button" 
+            className ="bg-background text-white p-3 rounded-md mt-3 absolute top-4 right-4 hover:bg-blue-400" 
+            onClick={(e) => toUserFunc(e, navigate)}>PFP</button>
     </div>
         
             
-        // <div className="flex h-screen">
-        //     {/* Button to Toggle Sidebar*/}
-        //     <button
-        //         className={`absolute top-4 ${isSidebarVisible ? 'right-4' : 'left-4'} bg-background hover:bg-blue-400 text-white px-4 py-2 rounded`}
-        //         onClick={toggleSidebar}
-        //     >
-        //         {isSidebarVisible ? 'x' : 'Logo'}
-        //     </button>
-        //     {isSidebarVisible && (
-        //                 <div className="w-4/10 bg-background p-4 justify-end">
-        //                 <h1 className="text-xl font-bold">Dashboard</h1>
-        //                 {/* <p>This column takes up 20% of the screen width.</p> */}
-
-        //                 <div className="mt-auto">
-        //                         <button
-        //                             id="logoutButton"
-        //                             className="button is-info px-4 py-2 hover:bg-blue-400"
-        //                             onClick={() => onLogout(navigate)}
-        //                         >
-        //                             Logout
-        //                         </button>
-        //                 </div>
-        //             </div>
-        //     )}
-
-        //     {/*TODO:  Weird case where when i try to add the table there's a chance that none of the data show up*/}
-        //     <div className ={"flex-grow bg-background p-4 flex flex-col  ${isSidebarVisible ? 'w-4/5' : 'w-full'}"}>  
-        //           {/* Top Section: Dashboard Title */}
-        //           <div>
-        //             {/* this doesn't show up when relaoding webpage? not storing userData? */}
-        //             {userData && (
-        //               <>
-        //                     {/* @ts-ignore - username is always defined */}
-        //                     <h1 className="text-2xl font-bold">{userData.username}'s Jobs</h1>
-        //                     <p className="text-gray-600">Here are your jobs:</p> {/*comment this out later */}
-        //                 </>
-        //             )}
-        //            </div>
-        //            <div>
-        //                         <h2>Jobs</h2>
-        //                         {/* @ts-ignore - username is always defined */}
-        //                        <Table userData={userData} updateUserData={updateUserData}/>
-        //                 </div> 
-        //     </div>
-        // </div>
+     
     );
 }
 
