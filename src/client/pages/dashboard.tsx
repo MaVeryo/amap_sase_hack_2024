4673ce0672c51cf03d6e41cff3ab8f56f34efa4b
pako490 <141786924+pako490@users.noreply.github.com>
2024-10-06 11:37:24 -0400

    table w/ sidebar  & found bug
    when there's already a prexisting entries in a user's table, there's a typeError that occurs
    still unsure about the cause

--- src/client/pages/dashboard.tsx
+++ src/client/pages/dashboard.tsx
@@ -46,52 +46,107 @@ function Dashboard() {
 
     console.log(userData);  
     return (
-        
         <div className="flex h-screen">
-            {/* Button to Toggle Sidebar*/}
-            <button
-                className={`absolute top-4 ${isSidebarVisible ? 'right-4' : 'left-4'} bg-background hover:bg-blue-400 text-white px-4 py-2 rounded`}
-                onClick={toggleSidebar}
-            >
-                {isSidebarVisible ? 'x' : 'Logo'}
-            </button>
+           
+            {/* Sidebar: 35% of the screen when visible */}
             {isSidebarVisible && (
-                        <div className="w-4/10 bg-background p-4 justify-end">
+                <div className="w-1/4 bg-background p-4 flex flex-col justify-between relative items-center">
+                    
+                    <div className = "flex items-cenT   er space-x-4"> 
                         <h1 className="text-xl font-bold">Dashboard</h1>
-                        {/* <p>This column takes up 20% of the screen width.</p> */}
-
-                        <div className="mt-auto">
-                                <button
-                                    id="logoutButton"
-                                    className="button is-info px-4 py-2 hover:bg-blue-400"
-                                    onClick={() => onLogout(navigate)}
-                                >
-                                    Logout
-                                </button>
+                        {/* Toggle Sidebar Button */}
+                        <button
+                            className="absolute top-4 right-4 bg-background hover:bg-blue-400 text-white px-4 py-2 rounded"
+                            onClick={toggleSidebar}
+                        >
+                            x
+                        </button>
+                        {/* Other sidebar content */}
+                        <div className="mt-10">
+                            <button
+                                id="logoutButton"
+                                className="bg-background hover:bg-blue-400 text-white px-4 py-2 rounded"
+                                onClick={() => onLogout(navigate)}
+                            >
+                                Logout
+                            </button>
                         </div>
                     </div>
+                </div>
             )}
+        <div className="flex-col">
+        {/* Button when the sidebar is hidden, positioned top-left of the screen */}
+            {!isSidebarVisible && (
+                <button
+                    className="absolute top-4 left-4 bg-background hover:bg-blue-400 text-white px-4 py-2 rounded"
+                    onClick={toggleSidebar}
+                >
+                    Logo
+                </button>
+            )}
+        {userData && (
+            <>
+                {/* @ts-ignore - username is always defined */}
+                <h1>{userData.username}'s Jobs</h1>
 
-            {/*TODO:  Weird case where when i try to add the table there's a chance that none of the data show up*/}
-            <div className ={"flex-grow bg-background p-4 flex flex-col  ${isSidebarVisible ? 'w-4/5' : 'w-full'}"}>  
-                  {/* Top Section: Dashboard Title */}
-                  <div>
-                    {/* this doesn't show up when relaoding webpage? not storing userData? */}
-                    {userData && (
-                      <>
-                            {/* @ts-ignore - username is always defined */}
-                            <h1 className="text-2xl font-bold">{userData.username}'s Jobs</h1>
-                            <p className="text-gray-600">Here are your jobs:</p> {/*comment this out later */}
-                        </>
-                    )}
-                   </div>
-                   <div>
-                                <h2>Jobs</h2>
-                                {/* @ts-ignore - username is always defined */}
-                               <Table userData={userData} updateUserData={updateUserData}/>
-                        </div> 
-            </div>
-        </div>
+                <AddJobForm userData={userData} setUserData={setUserData}/>
+
+                <div>
+                    <h2>Jobs</h2>
+                    <Table userData={userData} updateUserData={updateUserData}/>
+                </div>
+            </>
+        )}
+        
+    </div>
+    </div>
+        
+            
+        // <div className="flex h-screen">
+        //     {/* Button to Toggle Sidebar*/}
+        //     <button
+        //         className={`absolute top-4 ${isSidebarVisible ? 'right-4' : 'left-4'} bg-background hover:bg-blue-400 text-white px-4 py-2 rounded`}
+        //         onClick={toggleSidebar}
+        //     >
+        //         {isSidebarVisible ? 'x' : 'Logo'}
+        //     </button>
+        //     {isSidebarVisible && (
+        //                 <div className="w-4/10 bg-background p-4 justify-end">
+        //                 <h1 className="text-xl font-bold">Dashboard</h1>
+        //                 {/* <p>This column takes up 20% of the screen width.</p> */}
+
+        //                 <div className="mt-auto">
+        //                         <button
+        //                             id="logoutButton"
+        //                             className="button is-info px-4 py-2 hover:bg-blue-400"
+        //                             onClick={() => onLogout(navigate)}
+        //                         >
+        //                             Logout
+        //                         </button>
+        //                 </div>
+        //             </div>
+        //     )}
+
+        //     {/*TODO:  Weird case where when i try to add the table there's a chance that none of the data show up*/}
+        //     <div className ={"flex-grow bg-background p-4 flex flex-col  ${isSidebarVisible ? 'w-4/5' : 'w-full'}"}>  
+        //           {/* Top Section: Dashboard Title */}
+        //           <div>
+        //             {/* this doesn't show up when relaoding webpage? not storing userData? */}
+        //             {userData && (
+        //               <>
+        //                     {/* @ts-ignore - username is always defined */}
+        //                     <h1 className="text-2xl font-bold">{userData.username}'s Jobs</h1>
+        //                     <p className="text-gray-600">Here are your jobs:</p> {/*comment this out later */}
+        //                 </>
+        //             )}
+        //            </div>
+        //            <div>
+        //                         <h2>Jobs</h2>
+        //                         {/* @ts-ignore - username is always defined */}
+        //                        <Table userData={userData} updateUserData={updateUserData}/>
+        //                 </div> 
+        //     </div>
+        // </div>
     );
 }
 
