c14d413804ff7b1b7eb6d96bd65b2984a3a7a4ac
Alden Cutler <45373771+AldenCutler@users.noreply.github.com>
2024-10-06 21:48:10 -0400

    add About page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import {BrowserRouter as Router, Routes, Route, HashRouter} from 'react-router-dom';
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
-import Navigation from "../client/components/Naviagtion";
+import Navigation from "./components/Naviagtion";
 import Profile from "./pages/Profile";
+import { About } from "./pages/About";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/" element={<Login />} />
                     <Route  path="/dashboard" element={<Dashboard />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="/about" element={<About/>} />
                 </Routes>
             </HashRouter>
 
